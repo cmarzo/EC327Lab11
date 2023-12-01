4be4aa2e0d445d26b70a3861e52361cf70dd16a1
cmarzo <151081395+cmarzo@users.noreply.github.com>
2023-12-01 11:02:05 -0500

    Copilot Code

---
+++ Library.cpp
@@ -0,0 +1,43 @@
+//include headers and libraries
+#include "Library.h"
+#include <iostream>
+
+//implement class functions
+Library::Library() {
+    for (int i = 0; i < 10; ++i) {
+        books[i] = "";
+    } 
+}
+
+bool Library::addBook(string bookName) { 
+    for (int i = 0; i < 10; ++i) {
+        if (books[i] == bookName) {
+            return false; 
+        }
+}
+    for (int i = 0; i < 10; ++i) {
+        if (books[i] == "") {
+            books[i] = bookName;
+            return true; 
+        }
+}
+    return false; 
+}
+
+bool Library::removeBook(string bookName) {
+    for (int i = 0; i < 10; ++i) {
+        if (books[i] == bookName) {
+            books[i] = "";
+            return true; 
+        }
+    }
+    return false; 
+}
+
+void Library::print() {
+    for (int i = 0; i < 10; ++i) {
+        if (books[i] != "") {
+            cout << books[i] << endl;
+        }
+    }
+}
